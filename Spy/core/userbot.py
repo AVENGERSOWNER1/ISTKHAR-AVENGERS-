9fbb07335a7c5de26c58c0d6c3a610ff6284a70b
Gᴏᴅғᴀᴛʜᴇʀ <104144725+mrtosumemon@users.noreply.github.com>
2025-02-11 13:53:52 +0530

    Update userbot.py

--- Spy/core/userbot.py
+++ Spy/core/userbot.py
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("Dil_Feelings_Will1")
-                await self.two.join_chat("Dil_Feelings_Will1")
+                await self.two.join_chat("THE_INCRICIBLE")
+                await self.two.join_chat("ZOYU_SUPPORT")
             except:
                 pass
             assistants.append(2)
