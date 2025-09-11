600840e283304a09637bc8aaaef642864b7d7262
UTTAM RATHORE <uk977061@gmail.com>
2025-09-11 16:51:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,9 @@ COMMAND_HANDLER = getenv("COMMAND_HANDLER", "! / .").split()
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
+VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
+API_KEY = getenv("API_KEY", None) # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
